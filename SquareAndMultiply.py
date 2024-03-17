48c15c4da5594cf2be7f242bd71f6682ecea7982
Rangerbear <41147501+XiongWenye@users.noreply.github.com>
2024-03-17 21:05:13 +0800

    Add files via upload

---
+++ SquareAndMultiply.py
@@ -0,0 +1,12 @@
+def SquareAndMutilpy(a, e, n):
+    b = bin(e)[2:]
+    b = b[::-1]
+    ans = 1
+    for i in range(len(b)):
+        if b[i] == '1':
+            ans = (a * ans) % n
+        a = (a * a) % n
+    return ans
+
+base, exponent, modulus = map(int, input().split())
+print(SquareAndMutilpy(base, exponent, modulus))
